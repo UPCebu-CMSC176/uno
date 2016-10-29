214f9c55d1f13bbaa4f5a76c639ceb3501a3c5c7
cooolpiiink <trinityquirante11@gmail.com>
2016-10-29 18:34:07 +0800

    Activityy
    Week 2

---
+++ Game.hs
@@ -0,0 +1,21 @@
+module Game where
+
+import Common
+import System.Console.ANSI (Color(Black, Red, Green, Yellow, Blue, Magenta, Cyan, White))
+
+initGame :: Int -> State
+
+-- TODO: Implement a method to initialize a new game given n players
+initGame n = State { players = generateHumanPlayers n,
+                     deck = Common.fullDeck,
+                     d_stack = [ ] }
+		
+generateHumanPlayers :: Int -> [Player]
+generateHumanPlayers n 
+				| (n > 0) = [HPlayer {name = "Player" ++ show n, hand = [Card {color = Black, value = One}]}] ++ generateHumanPlayers (n-1)
+				| otherwise = [ ]
+							
+
+-- TODO: Implement a method to setup the game
+setupGame :: State -> IO State
+setupGame gs = return (gs)
